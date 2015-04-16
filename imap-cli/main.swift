7daf10d6cfe928bd2a3f5362511e1c080cc64960
Are Digranes <aredigranes@Ares-MacBook-Pro.local>
2015-04-16 21:20:32 +0200

    Initial Commit

---
+++ imap-cli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  imap-cli
+//
+//  Created by Are Digranes on 16.04.15.
+//  Copyright (c) 2015 Are Digranes. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
